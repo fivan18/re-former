d56c2e99676039d8d2dfff4496f2daa9494478fc
Ivan <fivanunam@gmail.com>
2020-03-02 10:52:11 -0600

    add edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update 
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to root_url
+  end
+
   private
 
   def user_params
