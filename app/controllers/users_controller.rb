7696d57940a181fdcd981b38053f13f8e9520367
certilremy <certilremy@gmail.com>
2020-03-02 11:17:04 -0500

    display errors messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
@@ -16,8 +16,7 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:username,:email,:password)
-    end
-
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
